86482dd57e6b883dbe2ecd635849da74db6378a4
Tanel Metshein <tanemets@tlu.ee>
2026-02-19 17:28:51 +0200

    3 tund
    add initial HTML and JavaScript files for local storage application

---
+++ 3_tund/script.js
@@ -0,0 +1,42 @@
+console.log("fail ühendatud")
+
+sessionStorage.setItem("eesnimi", "Tanel");
+
+let fname, lname, human;
+
+fname = document.getElementById("firstName");
+lname = document.getElementById("lastName");
+
+function saveData() {
+
+    human = {
+        firstname: fname.value,
+        lastname: lname.value
+    }
+
+    document.cookie = "eesnimi=" + fname.value;
+
+    sessionStorage.setItem("inimene", JSON.stringify(human));
+
+    sessionStorage.setItem("eesnimi", fname.value);
+    sessionStorage.setItem("perekonnanimi", lname.value);
+    localStorage.setItem("eesnimi", fname.value);
+    localStorage.setItem("perekonnanimi", lname.value);
+}
+
+function deletData() {
+    sessionStorage.removeItem("eesnimi");
+    sessionStorage.removeItem("perekonnanimi");
+    localStorage.removeItem("eesnimi");
+    localStorage.removeItem("perekonnanimi");
+}
+
+function loadData() {
+    /*document.getElementById("loadedData").innerHTML = sessionStorage.getItem("eesnimi") + " " + sessionStorage.getItem("perekonnanimi") + "<br>" + localStorage.getItem("eesnimi") + " " + localStorage.getItem("perekonnanimi") + "<br>" + sessionStorage.getItem("inimene");*/
+
+    document.getElementById("loadedData").innerHTML = JSON.parse(sessionStorage.getItem("inimene")).firstname + " " + JSON.parse(sessionStorage.getItem("inimene")).lastname;
+}
+
+document.getElementById("save").addEventListener("click", saveData);
+document.getElementById("delete").addEventListener("click", deletData);
+document.getElementById("load").addEventListener("click", loadData);
